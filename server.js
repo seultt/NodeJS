c63d7da561448b734c4510237a41cb9c86f654d1
seultt <seultt@gmail.com>
2017-08-31 18:55:47 +0900

    데이터 구조 추가 / CSS 기본 스타일

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 // 모듈 가져오기
 const express = require('express')
+const morgan = require('morgan')
+
+const data = [
+  {num: '1', header: '첫 게시글'}
+]
+
 const app = express()
+
+
 
 app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-  res.render('index.ejs')
+  res.render('index.ejs', {data})
 })
 
 app.listen(3000, () => {
