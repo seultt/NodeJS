bef365450174c9db38b1d9f7bccbd269b1042f4f
seultt <seultt@gmail.com>
2017-09-02 17:18:48 +0900

    댓글 내용 로드

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const bodyParser = require('body-parser')
 const data = [
   {num: 1, header: '첫 게시글', content: '우왕 신기하다'}
 ]
+const subData = [
+  {num: 1, name: '알라쑝', coment: '축핰ㅋ'},
+  {num: 1, name: '알라쑝', coment: '축핰ㅋ'},
+  {num: 1, name: '알라쑝', coment: '축핰ㅋ'}
+]
 
 const app = express()
-
 
 
 app.set('view engine', 'ejs')
 app.use('/static', express.static('public'))
 app.use(morgan('tiny'))
 app.use(bodyParser.urlencoded({ extended: false }))
-
-
 
 app.get('/', (req, res) => {
   res.render('index.ejs', {data})
@@ -26,6 +28,7 @@ app.get('/addPost', (req, res) => {
   res.render('addPost.ejs')
 })
 
+// addPost 내용 전송
 app.post('/addPost', (req, res) => {
   const header = req.body.header
   const content = req.body.content
@@ -34,18 +37,25 @@ app.post('/addPost', (req, res) => {
   res.redirect('/')
 })
 
+// content 내용, 댓글 로드
 app.get('/content/:num', (req, res) => {
   const num = parseInt(req.params.num)
-  const matched = [...data].find(item => item.num === num)
+  const matched = data.find(item => item.num === num)
+  const subMatched = subData.filter(sitem => sitem.num === num)
   console.log(matched)
-  if (matched) {
-    res.render('content.ejs',{matched})
+  console.log(subMatched)
+  if (matched && subMatched) {
+    res.render('content.ejs',{matched, subMatched})
   } else {
     res.status(404)
     res.send('404 Not Found')
   }
 })
 
-app.listen(3001, () => {
+
+
+
+
+app.listen(3002, () => {
   console.log('listening...')
 })
