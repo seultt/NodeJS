c5e2d264e973eafdae65ffa5d2d0fc2b5ca75ac5
seultt <seultt@gmail.com>
2017-09-04 03:01:47 +0900

    each  & if else 함수

--- anonymousBoard_pug/server.js
+++ anonymousBoard_pug/server.js
@@ -1,12 +1,16 @@
 const express = require('express')
 const app = express()
 
+const data = [
+  {num: 1, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: Date()}
+]
+
 app.locals.pretty = true; //pug : jade express code pretty
 app.set('view engine', 'pug') 
 app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => {
-  res.render('index', {time:Date(), title:'Jade'})
+  res.render('index', {data})
 })
 
 app.listen(3000, () => {
