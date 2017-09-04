656d1f23f0763a18b8c1e9f8df93ac38937046e1
seultt <seultt@gmail.com>
2017-09-05 03:50:11 +0900

    게시글 전송

--- anonymousBoard_pug/server.js
+++ anonymousBoard_pug/server.js
@@ -1,5 +1,9 @@
 const express = require('express')
+var bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // 날짜 함수
 const d = new Date()
@@ -11,8 +15,8 @@ function gotdate(today){
 
 // data
 const data = [
-  {num: 1, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: gotdate(d)},
-  {num: 2, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: gotdate(d)}
+  {num: 1, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'},
+  {num: 2, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'}
 ]
 
 app.locals.pretty = true; //pug : jade express code pretty
@@ -24,6 +28,16 @@ app.get('/', (req, res) => {
 })
 app.get('/addPost', (req, res) => {
   res.render('addPost')
+})
+
+app.post('/addPost', (req, res) => {
+  const title = req.body.title
+  const user = req.body.user
+  const content = req.body.content
+  const num = data.length +1
+  const date = gotdate(d)
+  data.push({title, user, content, num, date })
+  res.redirect('/')
 })
 
 app.listen(3000, () => {
