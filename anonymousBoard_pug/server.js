ae50636e50bf33ceae341106418a50b69278635e
seultt <seultt@gmail.com>
2017-09-07 00:04:54 +0900

    cotent CSS적용

--- anonymousBoard_pug/server.js
+++ anonymousBoard_pug/server.js
@@ -18,6 +18,11 @@ const data = [
   {num: 1, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'},
   {num: 2, title: '첫 게시글을 축하드립니다.', user: '헤헷', content:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'}
 ]
+// comentData
+const commentData = [
+  {num: 1, user: '헤헷', comment:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'},
+  {num: 2, user: '헤헷', comment:'Lorem Ipsum is simply dummy text of the printing and', date: 'SEP 05'}
+]
 
 app.locals.pretty = true; //pug : jade express code pretty
 app.set('view engine', 'pug') 
@@ -30,21 +35,29 @@ app.get('/addPost', (req, res) => {
   res.render('addPost')
 })
 
+
+// addPost 게시글 추가
 app.post('/addPost', (req, res) => {
+  let num
+  if(!data.length){ num = 1 } else {
+    num = Math.max(...data.map(item => item.num))
+    num++
+  }
   const title = req.body.title
   const user = req.body.user
   const content = req.body.content
-  const num = data.length +1
   const date = gotdate(d)
   data.push({title, user, content, num, date })
   res.redirect('/')
 })
 
+// content 내용, 댓글 로드
 app.get('/content/:num', (req, res) => {
   const num = parseInt(req.params.num)
   const matched = data.find(item => item.num === num)
-  if(matched) {
-    res.render('content', {matched})
+  const commentMatched = commentData.filter(item => item.num === num)
+  if(matched && commentMatched) {
+    res.render('content', {matched, commentMatched})
   } else{
     res.status(404)
     res.send('Not defined 404 Error')
